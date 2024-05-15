3d02fd932e256150f3c49ff8d814370c38e91adb
Yusuftaya1 <105513549+Yusuftaya1@users.noreply.github.com>
2024-05-15 17:19:12 +0300

    Update localization_try2.py

--- map_upt_point/src/map_updater/map_updater/localization_try2.py
+++ map_upt_point/src/map_updater/map_updater/localization_try2.py
@@ -1,25 +1,17 @@
 import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import OccupancyGrid, Odometry
-<<<<<<< HEAD
 import numpy as np
 
 class Localization(Node):
     def __init__(self):
         super().__init__('localization')
-=======
-
-class Localization(Node):
-    def _init_(self):
-        super()._init_('localization')
->>>>>>> 8d5a401d0637cb29d204b4b7ecd67cd559cdbd1b
         self.map_data = None
         self.odom_data = None
         self.map_sub = self.create_subscription(OccupancyGrid, 'map', self.map_callback, 10)
         self.odom_sub = self.create_subscription(Odometry, 'odom', self.odom_callback, 10)
 
     def map_callback(self, msg):
-<<<<<<< HEAD
         map_list = list(msg.data)
         self.map_json = {
             "msg_type": "MAP",
@@ -31,18 +23,6 @@ class Localization(Node):
         }
         self.map_array = np.array(map_list).reshape(int(self.map_json["width"]), int(self.map_json["height"]))
         print(self.map_array.shape)
-=======
-        map_data = list(msg.data)
-        map_json = {
-            "msg_type": "MAP",
-            "width": msg.info.width,
-            "height": msg.info.height,
-            "resolution": msg.info.resolution,
-            "data": map_data,
-            "end": "*end\n"
-        }
-        self.map_data = map_json
->>>>>>> 8d5a401d0637cb29d204b4b7ecd67cd559cdbd1b
 
     def odom_callback(self, msg):
         odom_data = {
@@ -52,10 +32,6 @@ class Localization(Node):
             "w": msg.pose.pose.orientation.w,
             "end": "*end\n"
         }
-<<<<<<< HEAD
-=======
-        self.odom_data = odom_data
->>>>>>> 8d5a401d0637cb29d204b4b7ecd67cd559cdbd1b
 
     def transform_to_cell(self,x,y,resolution):
         self.reso=resolution
@@ -63,19 +39,9 @@ class Localization(Node):
         self.celly = int(y/self.reso)
         #self.cellw = 
         return self.cellx, self.celly #self.cellw
-<<<<<<< HEAD
     
     def update_map(self):
         cellx , celly = self.transform_to_cell(self.odom_data["x"],self.odom_data["y"],self.map_data["resolution"])#,odom_data["w"]
-=======
-
-    def update_map(self):
-        cellx , celly = self.transform_to_cell(self.odom_data["x"],self.odom_data["y"],self.map_data["resolution"])#,odom_data["w"]
-        ## create publisher
-        ## map array update procces
-        ## publish updated map
-    
->>>>>>> 8d5a401d0637cb29d204b4b7ecd67cd559cdbd1b
 
 def main(args=None):
     rclpy.init(args=args)
@@ -85,9 +51,5 @@ def main(args=None):
     rclpy.spin(localization)
     rclpy.shutdown()
 
-<<<<<<< HEAD
 if __name__ == '__main__':
-=======
-if __name__ == '_main_':
->>>>>>> 8d5a401d0637cb29d204b4b7ecd67cd559cdbd1b
     main()
