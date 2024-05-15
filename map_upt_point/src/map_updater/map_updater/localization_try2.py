867362f12f0536f8e1c64deac0267ad536e3b10b
Yusuftaya1 <105513549+Yusuftaya1@users.noreply.github.com>
2024-05-15 17:50:58 +0300

    Update localization_try2.py

--- map_upt_point/src/map_updater/map_updater/localization_try2.py
+++ map_upt_point/src/map_updater/map_updater/localization_try2.py
@@ -6,23 +6,20 @@ import numpy as np
 class Localization(Node):
     def __init__(self):
         super().__init__('localization')
-        self.map_data = None
-        self.odom_data = None
         self.map_sub = self.create_subscription(OccupancyGrid, 'map', self.map_callback, 10)
         self.odom_sub = self.create_subscription(Odometry, 'odom', self.odom_callback, 10)
 
     def map_callback(self, msg):
         map_list = list(msg.data)
-        self.map_json = {
+        map_json = {
             "msg_type": "MAP",
-            "width": msg.info.width,#112
-            "height": msg.info.height,#103
+            "width": msg.info.width,
+            "height": msg.info.height,
             "resolution": msg.info.resolution,
             "data": map_list,
             "end": "*end\n"
         }
-        self.map_array = np.array(map_list).reshape(int(self.map_json["width"]), int(self.map_json["height"]))
-        print(self.map_array.shape)
+        self.map_array = np.array(map_list).reshape(int(map_json["width"]), int(map_json["height"]))
 
     def odom_callback(self, msg):
         odom_data = {
@@ -32,16 +29,19 @@ class Localization(Node):
             "w": msg.pose.pose.orientation.w,
             "end": "*end\n"
         }
-
+        self.update_map(odom_data["x"],odom_data["y"])
+    
     def transform_to_cell(self,x,y,resolution):
         self.reso=resolution
         self.cellx = int(x/self.reso)
         self.celly = int(y/self.reso)
-        #self.cellw = 
-        return self.cellx, self.celly #self.cellw
+        return self.cellx, self.celly 
     
-    def update_map(self):
-        cellx , celly = self.transform_to_cell(self.odom_data["x"],self.odom_data["y"],self.map_data["resolution"])#,odom_data["w"]
+    def update_map(self,X=0,Y=0,R=0.05):
+        cellx , celly = self.transform_to_cell(X,Y,R)
+        print("cellx: ",cellx)
+        print("celly: ",celly)
+
 
 def main(args=None):
     rclpy.init(args=args)
