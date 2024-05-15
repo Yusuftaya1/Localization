34c1ded54b3f36a4935ebb601f81e29c85fc4ddf
Yusuftaya1 <105513549+Yusuftaya1@users.noreply.github.com>
2024-05-15 12:41:58 +0300

    1
    1

--- map_upt_point/src/map_updater/map_updater/gpt_gived.py
+++ map_upt_point/src/map_updater/map_updater/gpt_gived.py
@@ -1,13 +1,13 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
-from nav_msgs.msg import OccupancyGrid, MapMetaData , Odometry
+from nav_msgs.msg import OccupancyGrid, Odometry
 from geometry_msgs.msg import PoseStamped
-import numpy as np
 
 class MapSub(Node):
-    def __init__(self):
+    def __init__(self, localization):
         super().__init__('map_subscriber')
+        self.localization = localization
         self.subscription = self.create_subscription(OccupancyGrid, 'map', self.map_callback, 10)
 
     def map_callback(self, msg):
@@ -21,11 +21,12 @@ class MapSub(Node):
             "data": map_data,
             "end": "**********end***********\n"
         }
-        return map_json
-        
+        self.localization.set_map_data(map_json)
+
 class OdomSub(Node):
-    def __init__(self):
+    def __init__(self, localization):
         super().__init__('odom_subscriber')
+        self.localization = localization
         self.subscription = self.create_subscription(Odometry, 'odom', self.odom_callback, 10)
 
     def odom_callback(self, msg):
@@ -36,53 +37,52 @@ class OdomSub(Node):
             "w": msg.pose.pose.orientation.w,
             "end": "**********end***********\n"
         }
-        return odom_data
-        
-class Localization():
-    def __init__(self, map_json=None, odom_data=None):
+        self.localization.set_odom_data(odom_data)
+
+class Localization(Node):
+    def __init__(self):
+        super().__init__('localization')
+        self.map_data = None
+        self.odom_data = None
+
+    def set_map_data(self, map_json):
         self.map_data = map_json
+        self._map_data()
+
+    def set_odom_data(self, odom_data):
         self.odom_data = odom_data
+        self._odom_data()
 
     def _map_data(self):
         print("Map Data:")
-        print("Message Type:", self.map["msg_type"])
-        print("Width:", self.map["width"])
-        print("Height:", self.map["height"])
-        print("Resolution:", self.map["resolution"])
-        print("Data:", self.map["data"])
-        print("End:", self.map["end"])
+        print("Message Type:", self.map_data["msg_type"])
+        print("Width:", self.map_data["width"])
+        print("Height:", self.map_data["height"])
+        print("Resolution:", self.map_data["resolution"])
+        print("Data:", self.map_data["data"])
+        print("End:", self.map_data["end"])
 
     def _odom_data(self):
         print("Odometry Data:")
-        print("Message Type:", self.odom["msg_type"])
-        print("X:", self.odom["x"])
-        print("Y:", self.odom["y"])
-        print("W:", self.odom["w"])
-        print("End:", self.odom["end"])
-
-    def transform_to_cell(self,x,y,resolution):
-        self.reso=resolution
-        self.cellx = int(x/self.reso)
-        self.celly = int(y/self.reso)
-        return self.cellx,self.celly
-    
-    def update_map(self):
-        pass
+        print("Message Type:", self.odom_data["msg_type"])
+        print("X:", self.odom_data["x"])
+        print("Y:", self.odom_data["y"])
+        print("W:", self.odom_data["w"])
+        print("End:", self.odom_data["end"])
 
 def main(args=None):
     rclpy.init(args=args)
 
-    # Both subscriptions are handled by Localization now
-    map=MapSub()
-    odom=OdomSub()
-    local = Localization(map_json=map,odom_data=odom)
+    localization = Localization()
+    map_sub = MapSub(localization)
+    odom_sub = OdomSub(localization)
 
     executor = MultiThreadedExecutor()
-    executor.add_node(odom)
-    executor.add_node(map)
-    executor.add_node(local)
+    executor.add_node(odom_sub)
+    executor.add_node(map_sub)
+    executor.add_node(localization)
 
-    rclpy.spin(executor=executor)
+    rclpy.spin(executor)
     rclpy.shutdown()
 
 if __name__ == '__main__':
